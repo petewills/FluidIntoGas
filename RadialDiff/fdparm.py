Fixed the bug that gave no change with radius. It was wrong units for tvar. Corrected in fdparm simply by multiplying by a day in seconds. That made it go to real physical times and spread the diffusion. Still need to connect all the time segments and double check the tvars; Then make the plot work

RadialDiff/fdparm.py
@@ -40,19 +40,22 @@ def getflow(nday, q_shutintime, figno=50):
     :return:
     """
 
+    day = 3600.0 * 24.0
     # These arrays will be used to control the odeint's
     qvar =      [ 0.0, 300.0, 500.0, 250.0, 0.0]                # flow rates throughout the simulation
-    tvar =      [ 0,      2,    70,    75,  85]                 # Times where the flow rates were initiated
+    tvar =      [ 0.2,      2,    70,    75,  85]               # Times where the flow rates were initiated. DONT START AT ZERO TIME
     dtvar =     [    0.2,   1.0,   1.0,  1.0]                   # step interval for getting pressures, in days. Oversample startup
     ind_shut =  2                                               # we shutin at index 2. modifies boundary condition
     nvar = len(tvar)
+    tvard, dtvard = [], []
+    for i in range(len(tvar)):
+        tvard.append(tvar[i] * day)
+    for i in range(len(dtvar)):
+        dtvard.append(dtvar[i] * day)
     times_ode, q_ode = [], []
     for i in range(len(dtvar)):
-        times_ode.append(np.arange( tvar[i], tvar[i+1], dtvar[i]))
+        times_ode.append(np.arange(tvard[i], tvard[i+1], dtvard[i]))
         q_ode.append(np.ones(len(times_ode[i])) * qvar[i])
-
-    print times_ode
-
 
     if len(qvar) != len(tvar):
         print 'Error: qvar and tvar need to be same length'
@@ -66,7 +69,7 @@ def getflow(nday, q_shutintime, figno=50):
     day = 0
     for i in range(nvar-1):
         flow = qvar[i]
-        for j in range(tvar[i], tvar[i+1]):
+        for j in range(int(tvar[i]), int(tvar[i+1])):
             x.append(day)
             y.append(flow)
             day += 1
@@ -76,7 +79,7 @@ def getflow(nday, q_shutintime, figno=50):
 
     # Plot the flow
     plt.figure(figno)
-    plt.plot(tvar, qvar, 'ro')
+    plt.plot(tvard, qvar, 'ro')
     plt.plot(x, y, 'k-')
     for i in range(len(dtvar)):
         plt.plot(times_ode[i], q_ode[i], 'ko-', markersize=3)

RadialDiff/library.py
@@ -93,6 +93,7 @@ def linesolve(lr, gp, tvec, fr):
     # Loop over separate odeints. Note that one is shutin (ind_shutin)
     h0 = 0.001
     y = []
+    # print 'bef times: ', fdprm.tvals_bef[-1]
     print 'shut index:', fdprm.ind_shut
     for i in range(fdprm.ind_shut):
         print 'times: ', fdprm.times_ode[i]
@@ -100,14 +101,17 @@ def linesolve(lr, gp, tvec, fr):
             times = fdprm.times_ode[i] - fdprm.times_ode[i-1][-1]
         else:
             times = fdprm.times_ode[0]
-        print 'initial: ',  y0/1000000
+        print 'times, old times: ',  fdprm.times_ode[i], fdprm.tvals_bef
         y_tmp, output = odeint(rhs, y0, times, h0=h0, hmax=2000.0, mxstep=2000, full_output=True, args=(lr, tvec, gp, 0.0,))
         y0 = y_tmp[-1]                          # Next is initialized with last step in this.
         print 'final: ',  y0/1000000, np.shape(y_tmp)
         y.append(y_tmp)
     print 'Done  ODEINT up to shutin'
 
-    # some kind of problem getting pressure diffusion to higher radii.
+    # Looks like I fixed the problem by using correct units for tvar
+    #   1. now need to complete the iterations
+    #   2. DOUBLE CHECK that we are doing the right thing in tvars parms to odeint! eg what is zero time
+    #   3. make the plotting work
     sys.exit()
 
     y0 = y_tmp[len(y_bef) - 1, :]              # After the shutin, we have no more fluid flow in boundary condition